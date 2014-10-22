5ba7356da5fab745038299caff40dac00101236d
darksasas <stanislas.delacour@gmail.com>
2014-10-22 22:54:48 +0200

    Day2
    Creation de upload()
    Création du template upload
    
    ToDo:
    Faire le "parse" du fichier CSV avec l'enregistrement dans la BDD

---
+++ immomemo_templates/upload.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<form name="upload_csv" action="upload" method="POST">
+	<label>Filename :</label>
+	<input type="text" name="filename" placeholder="exemple.csv"/>
+	<br>
+	<br>
+	<label>User's $uid :</label>
+	<input type="text" name="user_id" placeholder="integer"/>
+	<br>
+	<br>
+	<input type="submit" value="Submit">
+</form>
