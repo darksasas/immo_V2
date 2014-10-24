fd614c3dd03532fd71f591f313002c0e692d4080
darksasas <stanislas.delacour@gmail.com>
2014-10-25 01:04:55 +0200

    Day3
    - Progrès dans la fonction upload()
    - Modification de la table immo_buildings

--- BDD/immo_buildings.sql
+++ BDD/immo_buildings.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 21 Octobre 2014 à 21:28
+-- Généré le: Ven 24 Octobre 2014 à 22:57
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `immo_buildings` (
   `im_id` int(11) NOT NULL AUTO_INCREMENT,
   `str_id` int(11) NOT NULL,
   `im_nbr` varchar(20) NOT NULL,
+  `im_nbr2` varchar(20) NOT NULL,
   PRIMARY KEY (`im_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
