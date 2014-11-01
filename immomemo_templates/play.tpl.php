aaea083e443f122aa6e4a7e2a1e1aea9630e54f1
darksasas <stanislas.delacour@gmail.com>
2014-11-01 21:30:56 +0100

    Day6
    - Début de mise en place des fonctions principales

--- immomemo_templates/play.tpl.php
+++ immomemo_templates/play.tpl.php
@@ -27,6 +27,7 @@
     // Control Data
     var hostname = '<?= $base_url ?>';
     var screen_size;
+    var online;
 
 
     var streets = <?php echo json_encode($user_datas['streets']) ?>;
@@ -34,6 +35,8 @@
     var immeubles = <?php echo json_encode($user_datas['immeubles']) ?>;
 
     var habitants = <?php echo json_encode($user_datas['habitants']) ?>;
+
+    var user_timestamp = <?php echo ($user_datas['user_timestamp']) ?>;
 
     var changes = {};
 
@@ -66,6 +69,141 @@
         return true;
     }
 
+    function data_storage(type){
+
+        if (type='store') {
+            localStorage.setItem('streets',JSON.stringify(streets));
+            localStorage.setItem('immeubles',JSON.stringify(immeubles));
+            localStorage.setItem('habitants',JSON.stringify(habitants));
+            //localStorage.setItem('user_timestamp', Math.round(new Date().getTime() / 1000));
+            localStorage.setItem('user_timestamp', user_timestamp);
+        }
+        else if (type='load') {
+            streets = JSON.parse(localStorage.getItem("streets"));
+            immeubles = JSON.parse(localStorage.getItem("immeubles"));
+            habitants = JSON.parse(localStorage.getItem("habitants"));
+        }
+        else if (type=="reset") {
+            localStorage.clear();
+            //$('body').empty();
+            //location.reload();
+        };
+    }
+
+    function set_webapp(){
+
+        if (!online) { 
+            popup('offline_stop');
+            return;
+        };
+
+        // On check le timestamp
+        if (localStorage.getItem('user_timestamp')=== null) { // 1er chargement de l'app
+            data_storage('store');
+            // Pas besoin de vérification du timestamp
+        }
+        else{
+            check_timestamp('start_webapp');
+        }
+    }
+
+    function ajax(type, data){
+            
+        if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp=new XMLHttpRequest();
+        } 
+        else{// code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function(){
+            if (xmlhttp.readyState==1) {
+            }
+            else if (xmlhttp.readyState==4 && xmlhttp.status==200){
+                ajax_result = xmlhttp.responseText;
+                if (type=='request_access_timestamp') {
+                    if (ajax_result!='DENIED') {
+                        localStorage.setItem('access_timestamp', ajax_result);
+                        set_webapp('check_products_timestamp');
+                    }
+                    else{
+                        popup('user_denied');
+                    }  
+                }
+                else if (type=="check_products_timestamp"){
+                    if (ajax_result == 'OK') {
+                        prod_storage('load');
+                    }
+                    else if (ajax_result=='DENIED') {
+                        popup('user_denied');
+                    }
+                    else{
+                        prod_storage('update', ajax_result);
+                    }
+                } 
+            }
+        }
+        if(type=='request_access_timestamp'){
+            xmlhttp.open("POST","<?= $base_url ?>/webapp/ajax/request_access_timestamp",true);
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send("user_id="+user_id);
+        }
+        else if (type=='check_products_timestamp'){
+            xmlhttp.open("POST","<?= $base_url ?>/webapp/ajax/check_products_timestamp",true);
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send("user_id="+user_id+"&timestamp="+data);
+        }
+        else if (type=='sendFavs'){
+            xmlhttp.open("POST","<?= $base_url ?>/webapp/ajax/sendFavs",true);
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send("user_id="+user_id+"&email="+data['email']+"&favs="+(data['type']=='all'? JSON.stringify(favs) : data['type']));
+        }
+    }
+
+    function popup(type){
+        
+        ('#popup_screen').empty();
+
+        if (type='offline_stop') {
+            $('#popup_screen').append('Attention, vous devez être connecté pour lancer l\'application !');
+        };
+    }
+
+    function check_timestamp(type){
+        
+        if (type='start_webapp') {
+            $.ajax({
+                url: hostname+"/immomemo/ajax/timestamp",
+                type: "POST",
+                async: false, // Mode synchrone
+                data: ({
+                    user_timestamp: localStorage.getItem("user_timestamp"),
+                    //format: $("#format").val()
+                }),
+                dataType: "json",
+                complete: function(data){
+                    //console.log('check_timestamp: '+data.responseText);
+                    console.log(data);
+
+                    // En fonction de la réponse
+                    if (data.responseText == 'SERVER_OLD')
+                        check_timestamp('SERVER_OLD');
+                    else if (data.responseText == 'NO_CHANGE') {}
+                        // Do nothing
+                    else { 
+                        // On enregistre les données envoyées par le serveur
+                        streets = data.responseJSON.streets;
+                        immeubles = data.responseJSON.immeubles;
+                        habitants = data.responseJSON.habitants;
+                        user_timestamp = data.responseJSON.user_timestamp;
+                        data_storage('store');
+                    }
+                }
+            });
+        }
+        else if (type='SERVER_OLD') {
+            // On upload les données (ou les changements ?) de la webapp vers le webserver
+        };
+    }
 
     </script>
 
@@ -74,23 +212,82 @@
 <body>
 
     <style>
+        
+        /*********/
+        /*Screens*/
+        /*********/
+        #popup_screen{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 90;
+            overflow: hidden;
+            -webkit-overflow-scrolling: touch;
+        }
+        #select_screen{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 90;
+            overflow: hidden;
+            -webkit-overflow-scrolling: touch;
+
+            background-color: blue;
+        }
+        #street_screen{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 90;
+            overflow: hidden;
+            -webkit-overflow-scrolling: touch;
+        }
+        #habitants_screen{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 90;
+            overflow: hidden;
+            -webkit-overflow-scrolling: touch;
+        }
+        #edit_screen{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 90;
+            overflow: hidden;
+            -webkit-overflow-scrolling: touch;
+        }
+
+
     </style>
 
     <div id="loader" style="display:none">
-        <div id="loading_bg">&nbsp;</div><p id="loading_img"><img src="<?= $base_url ?>/sites/all/modules/symrise/img/processing.png" width="200px"></p>
     </div>
 
-    <div id="popup_screens">
+    <div id="popup_screen">
     </div>
 
-    <div id="select_univers">
+    <div id="select_screen">
     </div>
 
-    <div id="summary">
+    <div id="street_screen">
     </div>
 
-    <div id="contents">
-    	yop play
+    <div id="habitants_screen">
+    </div>
+
+    <div id="edit_screen">
     </div>
 
 	<script type="text/javascript" src="<?= $base_url ?>/sites/all/libraries/JS/jquery-1.11.0.min.js"></script>
@@ -106,6 +303,9 @@
         	//localStorage.clear();
 
             screen_size = get_screen_size();
+            online = isonline();
+
+            set_webapp();
 
             //**************************
             // Buble "Add to Homescreen"
